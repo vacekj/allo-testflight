44a4b310f0e2b29b42f0de1101c4e1d753b60b8e
Atris <vacekj@outlook.com>
2023-06-06 16:51:41 +0200

    feat: wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
-import { useAccount } from 'wagmi'
+import {useAccount} from 'wagmi'
 
-import { Account } from './components/Account'
-import { Connect } from './components/Connect'
-import { Counter } from './components/Counter'
-import { NetworkSwitcher } from './components/NetworkSwitcher'
+import {Account} from './components/Account'
+import {Connect} from './components/Connect'
+import {NetworkSwitcher} from './components/NetworkSwitcher'
+
+import {Deployer} from "./components/Deployer";
 
 export function App() {
-  const { isConnected } = useAccount()
+    const {isConnected} = useAccount()
 
-  return (
-    <>
-      <h1>wagmi + Vite</h1>
-
-      <Connect />
-
-      {isConnected && (
+    return (
         <>
-          <Account />
-          <hr />
-          <Counter />
-          <hr />
-          <NetworkSwitcher />
+            <h1>Allo Protocol Testflight</h1>
+
+            <Connect/>
+
+            {isConnected && (
+                <>
+                    <Account/>
+                    <NetworkSwitcher/>
+                    <Deployer/>
+                </>
+            )}
         </>
-      )}
-    </>
-  )
+    )
 }
---
+++ src/components/Deployer.tsx
@@ -0,0 +1,8 @@
+export function Deployer() {
+    return <div>
+        <button onClick={() => {
+
+        }}>Deploy
+        </button>
+    </div>
+}
