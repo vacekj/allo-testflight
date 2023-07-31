2f8f6fe965ef00513431aabb2a2448ee0badf439
Atris <vacekj@outlook.com>
2023-07-31 18:10:35 +0200

    feat: eureka!

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,13 @@ import { signPermitEIP2612 } from "./permit";
 const MRC_IMPL = "0x521bb00da4273e1882d2fb690388cad81bdd5e55";
 const groupedVotes: Record<Hex, Hex[]> = {
   "0x894D5d1E65B9C51671e93E2e9a159C90d1a455d1": [
-    "0x00000000000000000000000011fe4b6ae13d2a6055c8d9cf65c55bac32b5d8440000000000000000000000000000000000000000000000000de0b6b3a76400000000000000000000000000005f390415db0f7d4d336095f3fd266d6b3b616e7ae280d64d9f7778747f0ff29976a933a73849c759a95cf0e8fd2db18cb495933a0000000000000000000000000000000000000000000000000000000000000000",
-    "0x00000000000000000000000011fe4b6ae13d2a6055c8d9cf65c55bac32b5d8440000000000000000000000000000000000000000000000000de0b6b3a7640000000000000000000000000000668610ff020ba6326d1f39d58464f2a5892a7cbe2ae9c96f8c81a963d5cc2d52b50f90e7d95e5ddee68c81c84e9d609a0a17f3930000000000000000000000000000000000000000000000000000000000000001",
+    "0x000000000000000000000000baa146619512b97216991ba37ae74de213605f8e0000000000000000000000000000000000000000000000000de0b6b3a76400000000000000000000000000005f390415db0f7d4d336095f3fd266d6b3b616e7ae280d64d9f7778747f0ff29976a933a73849c759a95cf0e8fd2db18cb495933a0000000000000000000000000000000000000000000000000000000000000000",
+    "0x000000000000000000000000baa146619512b97216991ba37ae74de213605f8e0000000000000000000000000000000000000000000000000de0b6b3a7640000000000000000000000000000668610ff020ba6326d1f39d58464f2a5892a7cbe2ae9c96f8c81a963d5cc2d52b50f90e7d95e5ddee68c81c84e9d609a0a17f3930000000000000000000000000000000000000000000000000000000000000001",
   ],
 };
 
 const groupedAmounts: Record<Hex, bigint> = {
-  "0x894D5d1E65B9C51671e93E2e9a159C90d1a455d1": 2n,
+  "0x894D5d1E65B9C51671e93E2e9a159C90d1a455d1": (2n * (10n ** 18n)),
 };
 
 const rounds = Object.keys(groupedAmounts) as Hex[];
@@ -38,7 +38,7 @@ export function App() {
       Object.values(groupedVotes),
       rounds,
       Object.values(groupedAmounts),
-      2n,
+      Object.values(groupedAmounts).reduce((acc, b) => acc + b),
       XPZ,
       rsv.deadline,
       rsv.v,
@@ -71,7 +71,7 @@ export function App() {
             erc20Name: "Exprez",
             owner: address,
             spender: MRC_IMPL,
-            value: 2n,
+            value: Object.values(groupedAmounts).reduce((acc, b) => acc + b),
           });
           const [r, s, v] = [slice(rawPermit, 0, 32), slice(rawPermit, 32, 64), slice(rawPermit, 64, 65)];
           console.log(r, s, hexToNumber(v));
@@ -95,7 +95,15 @@ export function App() {
       >
         vote
       </Button>
+      <pre>
+        {prettify(data)}
+      </pre>
+
       <ConnectButton />
     </Container>
   );
+}
+
+function prettify(input: any) {
+  return JSON.stringify(input, null, 2);
 }
